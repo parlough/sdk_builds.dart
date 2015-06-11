e8438754ecacf7a63560911c243600f7ba9574ae
Kevin Moore <kevmoo@google.com>
2015-06-11 15:54:34 +0200

    moved to show download info

--- web/index.dart
+++ web/index.dart
@@ -1,21 +1,47 @@
 library sdk_builds.web.index;
 
+import 'dart:async';
+
 import "package:angular2/angular2.dart";
 import "package:angular2/src/reflection/reflection.dart" show reflector;
 import "package:angular2/src/reflection/reflection_capabilities.dart"
     show ReflectionCapabilities;
 
-const _greetings = const ['hello', 'howdy', 'cherrio'];
+import 'package:http/browser_client.dart';
+import 'package:sdk_builds/sdk_builds.dart';
 
 @Component(selector: "hello-app")
-@View(templateUrl: 'template.html')
+@View(templateUrl: 'template.html', directives: const [NgFor])
 class HelloComponent {
-  String greeting = _greetings.first;
+  final _dd = new DartDownloads(client: new BrowserClient());
+  bool working = false;
+  List<String> versionInfo = <String>[];
 
-  void changeGreeting() {
-    var nextIndex = (_greetings.indexOf(this.greeting) + 1) % _greetings.length;
+  void updateLatestVersion() {
+    _updateVersions();
+  }
 
-    this.greeting = _greetings[nextIndex];
+  Future _updateVersions() async {
+    if (working) return;
+    working = true;
+    try {
+      versionInfo.clear();
+      versionInfo.add('Working...');
+      var values = await Future.wait([_latest('stable'), _latest('dev')]);
+
+      await new Future.delayed(const Duration(seconds: 5));
+
+      versionInfo.clear();
+      versionInfo.addAll(values);
+    } finally {
+      working = false;
+    }
+  }
+  
+  Future<String> _latest(String channel) async {
+    var hash = await _dd.getVersionMap(channel, 'latest');
+
+    return '$channel\t${hash['version']}';
   }
 }
 
